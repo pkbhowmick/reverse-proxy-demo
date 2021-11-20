f1e91ac2336dcba922d002c15b58206a297438c2
Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>
2021-11-21 01:30:30 +0600

    Add health checker and L4 LB
    Signed-off-by: Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
@@ -13,11 +18,17 @@ import (
 
 const (
 	proxyAddr = "0.0.0.0:8000"
-	server    = "http://127.0.0.1:8080/"
+	MaxRetry  = 3
 )
 
-func getServerAddr() (*url.URL, error) {
-	url, err := url.Parse(server)
+var (
+	servers   []string        = []string{"http://127.0.0.1:8080", "http://127.0.0.1:8081"}
+	isHealthy map[string]bool = make(map[string]bool)
+	locker    sync.Mutex
+)
+
+func getServerAddr(svr string) (*url.URL, error) {
+	url, err := url.Parse(svr)
 	if err != nil {
 		return nil, err
 	}
@@ -25,36 +36,87 @@ func getServerAddr() (*url.URL, error) {
 }
 
 func proxy(w http.ResponseWriter, r *http.Request) {
-	svrURL, err := getServerAddr()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+	for try := 1; try <= MaxRetry; try++ {
+		svr := rand.Intn(len(servers))
+		svrURL, err := getServerAddr(servers[svr])
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		r.Host = svrURL.Host
+		r.URL.Host = svrURL.Host
+		r.URL.Scheme = svrURL.Scheme
+		r.RequestURI = ""
+
+		res, err := http.DefaultClient.Do(r)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		if res.StatusCode >= 300 && try < MaxRetry {
+			continue
+		}
+
+		// Copy server response header to client response header
+		for key, values := range res.Header {
+			for _, v := range values {
+				w.Header().Set(key, v)
+			}
+		}
+
+		w.WriteHeader(res.StatusCode)
+		io.Copy(w, res.Body)
 		return
 	}
-	r.Host = svrURL.Host
-	r.URL.Host = svrURL.Host
-	r.URL.Scheme = svrURL.Scheme
-	r.RequestURI = ""
+}
 
-	res, err := http.DefaultClient.Do(r)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
+func healthChecker(ctx context.Context) {
+	for {
+		for _, s := range servers {
+			healthPath := s + "/healthz"
+			res, err := http.Get(healthPath)
+			if err != nil {
+				isHealthy[s] = false
+				continue
+			}
+			if res.StatusCode == http.StatusOK {
+				locker.Lock()
+				isHealthy[s] = true
+				locker.Unlock()
+			} else {
+				locker.Lock()
+				isHealthy[s] = false
+				locker.Unlock()
+			}
+		}
+		time.Sleep(5 * time.Second)
 	}
+}
 
-	// Copy server response header to client response header
-	for key, values := range res.Header {
-		for _, v := range values {
-			w.Header().Set(key, v)
+func getUnheathyServer(w http.ResponseWriter, r *http.Request) {
+	locker.Lock()
+	svrs := make([]string, 0)
+	for s, ok := range isHealthy {
+		if !ok {
+			svrs = append(svrs, s)
 		}
 	}
-
-	w.WriteHeader(res.StatusCode)
-	io.Copy(w, res.Body)
+	locker.Unlock()
+	res := strings.Join(svrs, "\n")
+	res = "Down servers:\n" + res
+	w.Write([]byte(res))
 }
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Run healthChecker
+	go healthChecker(ctx)
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
@@ -64,9 +126,15 @@ func main() {
 
 	r.Get("/api/*", proxy)
 
-	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "OK")
+	r.Group(func(r chi.Router) {
+		// todo: add middleware to check admin authorization
+		r.Route("/admin", func(r chi.Router) {
+			r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprintf(w, "OK")
+			})
+			r.Get("/list-unhealthy-servers", getUnheathyServer)
+		})
 	})
 
 	log.Println("Proxy server is running on port 8000")
--- server/servers.go
+++ server/servers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,10 +12,22 @@ import (
 )
 
 const (
-	addr = "0.0.0.0:8080"
+	addr1 = "0.0.0.0:8080"
+	addr2 = "0.0.0.0:8081"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go StartServer1(ctx)
+	go StartServer2(ctx)
+
+	select {}
+
+}
+
+func StartServer1(ctx context.Context) {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
@@ -45,8 +58,47 @@ func main() {
 		fmt.Fprintf(w, "OK")
 	})
 
-	log.Println("Server is listening on port 8080")
-	if err := http.ListenAndServe(addr, r); err != nil {
-		log.Fatalln(err)
+	log.Println("Server 1 is listening on port 8080")
+	if err := http.ListenAndServe(addr1, r); err != nil {
+		log.Println(err)
 	}
+	ctx.Done()
+}
+
+func StartServer2(ctx context.Context) {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	r.Get("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Hello from server 2")
+	})
+
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK")
+	})
+
+	log.Println("Server 2 is listening on port 8081")
+	if err := http.ListenAndServe(addr2, r); err != nil {
+		log.Println(err)
+	}
+	ctx.Done()
 }
